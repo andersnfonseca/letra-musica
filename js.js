c4f3f8df1ad5d95a76c4a3b8adc037351922bf39
Anderson Assunção da Fonseca <36905253+andersnfonseca@users.noreply.github.com>
2021-10-23 06:16:23 -0500

    Add files via upload

---
+++ js.js
@@ -0,0 +1,22 @@
+const pegarLetraMusica = async() => {
+        
+        var artista = document.getElementById("artista").value;
+        var musica = document.getElementById("musica").value;
+        var key = `15fcead61e2ec42c7d8b170f6018fbe6`;
+        var URL = `https://api.vagalume.com.br/search.php?art=${artista}&mus=${musica}&apikey=${key}`
+
+        const dados = await fetch(URL);
+        const letraDaMusica = await dados.json();
+        console.log(letraDaMusica.mus[0].translate[0].text);
+
+        document.getElementById("cantor").innerText = letraDaMusica.art.name;
+        document.getElementById("letra").innerText = letraDaMusica.mus[0].text;
+        document.getElementById("traducao").innerText = letraDaMusica.mus[0].translate[0].text;
+        
+        limparCampos();
+}
+
+function limparCampos() {
+    document.getElementById('artista').value='';
+    document.getElementById('musica').value='';
+}
